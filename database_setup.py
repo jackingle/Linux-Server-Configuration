82d97682bf819d7d7b97531196fea15a9c39aa8a
sampleerror <jackingle@outlook.com>
2019-09-20 12:09:54 -0600

    Update login for OAuth2

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,3 @@
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -9,42 +8,33 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
-    name = Column(
-    String(80), nullable = False
-    )
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
-    id = Column(
-    Integer, primary_key = True
-    )
-
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name = Column(
-    String(80), nullable = False
-    )
 
-    id = Column(
-    Integer, primary_key = True
-    )
-
-    course = Column(String(250))
-
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
     description = Column(String(250))
-
     price = Column(String(8))
-
-    restaurant_id = Column(
-    Integer, ForeignKey('restaurant.id')
-    )
-
+    course = Column(String(250))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
-#We added this serialize function to be able to send JSON objects in a serializable format
+
     @property
     def serialize(self):
-
+       """Return object data in easily serializeable format"""
        return {
            'name'         : self.name,
            'description'         : self.description,
@@ -53,5 +43,11 @@ class MenuItem(Base):
            'course'         : self.course,
        }
 
+
+
 engine = create_engine('sqlite:///restaurantmenu.db')
+
+
 Base.metadata.create_all(engine)
+GET | 302 | 140 ms | GitHub.com
+GET | 200 | 95 ms
