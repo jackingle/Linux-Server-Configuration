2c3ecfc8c5b88133b956a6aca5c3612e83ee1fcf
sampleerror <jackingle@outlook.com>
2019-09-24 13:15:03 -0600

    Refactor:  Change Restaurant base to RPG based
    Substantial changes are necessary to the catalog in order to accomodate
    the requirements.  Instead of a restaurant with menus, the database
    contains Items with Equipment.  The biggest changes were to
    database_setup.  Template names have been changed to match.

--- database_setup.py
+++ database_setup.py
@@ -5,49 +5,49 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
-
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+    email = Column(String(250), nullable=False)
 
 
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Equipment(Base):
+    __tablename__ = 'equipment'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+    type = Column(String(250))
+    item_id = Column(Integer, ForeignKey('item.id'))
+    item = relationship(Item)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'type': self.type,
+        }
 
 
-
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///item.db')
 
 
 Base.metadata.create_all(engine)
-GET | 302 | 140 ms | GitHub.com
-GET | 200 | 95 ms
