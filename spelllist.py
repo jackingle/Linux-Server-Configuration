a4bc33c94c8b005e4aac93e52efb7b47b20708a0
Jack Ingle <jackingle@outlook.com>
2019-10-08 09:09:42 -0500

    Correct errors related to PEP8 formatting

--- spelllist.py
+++ spelllist.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for,
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from flask import flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -91,98 +91,93 @@ a sign-in method, currently the only sign in method for the
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-code = request.data
-"""
-This passes the authorization code into a credentials object if the validation
-passes.
-"""
-try:
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'http://localhost:8000'
         credentials = oauth_flow.step2_exchange(code)
-        print (credentials)
-except FlowExchangeError:
-        print ("FlowExchangeError")
+    except FlowExchangeError:
         response = make_response(
             json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-"""
-Retrieves and checks the access token.
-"""
-access_token = credentials.access_token
-url = (
-    'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
-    % access_token)
-h = httplib2.Http()
-result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
-if result.get('error') is not None:
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
-"""
-Matches the google verified user to the token in order to ensure they're the
-same person.
-"""
-gplus_id = credentials.id_token['sub']
-if result['user_id'] != gplus_id:
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-"""
-This verifies that the app is the correct app for OAuth2.
-"""
-if result['issued_to'] != CLIENT_ID:
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
         print ("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
-stored_access_token = getUserID('access_token')
-stored_gplus_id = login_session.get('gplus_id')
-if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps(
-            'Current user is already connected.'),
-            200)
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-login_session['access_token'] = credentials.access_token
-login_session['gplus_id'] = gplus_id
-userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-params = {'access_token': credentials.access_token, 'alt': 'json'}
-answer = requests.get(userinfo_url, params=params)
-data = answer.json()
-login_session['username'] = data['name']
-login_session['picture'] = data['picture']
-login_session['email'] = data['email']
-login_session['provider'] = 'google'
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
 
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
 
-"""
-Now that the OAuth has passed and data has been stored in the login_session
-object, pass that data into the getUserID function and verify the user is made
-or create one.
-Finally, flash a quick login screen.
-"""
-user_id = getUserID(data["email"])
-if not user_id:
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+# ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
+    # see if user exists, if it doesn't make a new one
+    print(data["email"])
+    user_id = getUserID(data["email"])
+    if not user_id:
         user_id = createUser(login_session)
-login_session['user_id'] = user_id
-output = ''
-output += '<h1>Welcome, '
-output += login_session['username']
-output += '!</h1>'
-flash("you are now logged in as %s" % login_session['username'])
-print ("done!")
-return output
-
+    login_session['user_id'] = user_id
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print ("done!")
+    return output
 
 """
 This function allows the user to disconnect from google and revokes the
@@ -202,8 +197,7 @@ def gdisconnect():
     print ('In gdisconnect access token is %s', access_token)
     print ('User name is: ')
     print (login_session['username'])
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s'
-    % login_session['access_token']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print ('result is ')
